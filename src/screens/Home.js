9d3fe05a8f8f85e843655e90702a04eeb0552df2
Dhanush <dhanushdevireddy@gmail.com>
2023-08-08 12:26:51 +0530

    Navbar completed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Form from "../components/Form";
+import { Container, Row } from "react-bootstrap";
 
 export default function Home() {
   return (
     <>
-    <Navbar/>
-    <div>Home</div>
-    <Footer/>
+      <Navbar />
+      <Container>
+        <Row>
+          <div className="fs-2">
+            Did you lose an item? Please upload its details here.
+          </div>
+        </Row>
+        <Row>
+          <Form />
+        </Row>
+        <Row>
+          <div className="fs-2">
+            Did you find an item? Please upload its details here.
+          </div>
+        </Row>
+        <Row>
+          <Form />
+        </Row>
+      </Container>
+      <Footer />
     </>
-    
-  )
+  );
 }
---
+++ src/screens/LostItems.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function LostItems() {
+  return (
+    <>
+    <Navbar/>
+    <div>LostItems</div>
+    <Footer/>
+    </>
+  )
+}
