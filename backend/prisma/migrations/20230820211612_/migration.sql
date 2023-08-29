cdc1d1da028433404aa06d5ea6ae024de5192d57
Dhanush <dhanushdevireddy@gmail.com>
2023-08-29 18:01:10 +0530

    Removed backend folder

--- backend/prisma/migrations/20230820211612_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `published` on the `Object` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Object" DROP COLUMN "published",
-ADD COLUMN     "resolved" BOOLEAN NOT NULL DEFAULT false;
