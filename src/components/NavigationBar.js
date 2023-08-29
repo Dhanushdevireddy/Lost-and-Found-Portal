f03f8d1913c0c676cf12a1acfdbf1c2971c63987
Dhanush <dhanushdevireddy@gmail.com>
2023-08-29 19:48:35 +0530

    "updated NavigationBar"

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 //import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -7,32 +7,36 @@ import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import { GoogleLogin } from '@react-oauth/google';
+import{
+  Link
+} from "react-router-dom"
 
 export default function NavigationBar() {
-
+  const [loggedIn] = useState(false)
   return (
     <Navbar expand="lg" className="bg-body-tertiary justify-content-between">
       <Container>
-        <Navbar.Brand href="/">lostNfound</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" eventKey="/">lostNfound</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/itemsLost">Lost Items</Nav.Link>
-            <Nav.Link href="/itemsFound">Found Items</Nav.Link>
-            <Nav.Link href="/itemsCollected">Collected Items</Nav.Link>
+            <Nav.Link as={Link} to="/" eventKey="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/itemsLost" eventKey="/itemsLost">Lost Items</Nav.Link>
+            <Nav.Link as={Link} to="/itemsFound" eventKey="/itemsFound">Found Items</Nav.Link>
+            <Nav.Link as={Link} to="/itemsCollected" eventKey="/itemsCollected">Collected Items</Nav.Link>
           </Nav>
-          <Nav.Link href="/userProfile" style={{ "margin-left": "20px" }}>
+          
+          {loggedIn?<><Nav.Link as={Link} to="/userProfile" eventKey="/userProfile" style={{ "margin-right": "20px" }}>
             User Profile
-          </Nav.Link>
-          <Button
+          </Nav.Link><Button
             variant="danger"
-            style={{ "margin-left": "20px","margin-right":"20px" }}
+            style={{ "margin-left": "20px"}}
             type="submit"
           >
             Logout
-          </Button>
-          <GoogleLogin onSuccess={()=>{}} />
+          </Button></>:<GoogleLogin onSuccess={(res)=>{console.log(res)}} onError={(error)=>{console.log(error)}}/>}
+          
+          
         </Navbar.Collapse>
       </Container>
     </Navbar>
