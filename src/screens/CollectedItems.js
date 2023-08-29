51b9f170cd5c977bad52cc24bdb1e88044b99579
Dhanush <dhanushdevireddy@gmail.com>
2023-08-29 17:57:08 +0530

    Commit

--- src/screens/CollectedItems.js
+++ src/screens/CollectedItems.js
@@ -3,16 +3,23 @@ import NavigationBar from "../components/NavigationBar";
 import Footer from "../components/Footer";
 import CardRendering from '../components/CardRendering'
 import SearchBar from "../components/SearchBar";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function CollectedItems() {
   return (
     <>
       <NavigationBar />
-      <SearchBar/>
-      <CardRendering/>
-    <CardRendering/>
-    <CardRendering/>
-    <CardRendering/>
+      <Container>
+        <Row>
+          <Col xs={12}>
+            <SearchBar />
+          </Col>
+        </Row>
+        <CardRendering xs={12} md={6} />
+        <CardRendering xs={12} md={6} />
+        <CardRendering xs={12} md={6} />
+        <CardRendering xs={12} md={6} />
+      </Container>
       <Footer />
     </>
   );
--- src/screens/FoundItems.js
+++ src/screens/FoundItems.js
@@ -3,17 +3,24 @@ import Footer from '../components/Footer'
 import NavigationBar from '../components/NavigationBar'
 import CardRendering from '../components/CardRendering'
 import SearchBar from '../components/SearchBar'
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function FoundItems() {
   return (
     <>
-    <NavigationBar/>
-    <SearchBar/>
-    <CardRendering/>
-    <CardRendering/>
-    <CardRendering/>
-    <CardRendering/>
-    <Footer/>
+    <NavigationBar />
+      <Container>
+        <Row>
+          <Col xs={12}>
+            <SearchBar />
+          </Col>
+        </Row>
+        <CardRendering xs={12} md={6} />
+        <CardRendering xs={12} md={6} />
+        <CardRendering xs={12} md={6} />
+        <CardRendering xs={12} md={6} />
+      </Container>
+      <Footer />
     </>
   )
 }
